db44dc0e12253eaf3e8febd75cb64b4639ce097b
Lucas C Oliveira <lucascoliveira819@gmail.com>
2020-10-19 21:39:17 -0300

    playtest results

--- src/Loader/Loader.gd
+++ src/Loader/Loader.gd
@@ -10,5 +10,5 @@ func _ready() -> void:
 
 func game_loaded(game_to_instance) -> void:
 	var game_ref = game_to_instance.instance()
-	print("game_loaded | game_ref: ", game_ref)
+	# print("game_loaded | game_ref: ", game_ref)
 	SignalManager.emit_signal(SignalManager.SIGNAL_GAME_LOADED, game_ref)
